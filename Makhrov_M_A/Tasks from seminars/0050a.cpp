e7fccf2137518c2c8c5ff4501da26883ea493f7f
MatveyMakhrov <113934714+MatveyMakhrov@users.noreply.github.com>
2022-10-11 13:38:47 +0300

    Create 0050a.cpp

---
+++ Makhrov_M_A/Tasks from seminars/0050a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+ 
+int main() {
+	int m(0), n(0), a1(2), a2(1), k(0); // a1 это длина доминошки, а a2 это ширина доминошки
+	std::cin >> m >> n;
+	k = m * n;
+	int s;
+	s = a1 * a2;
+	int q;
+	q = k / s;
+	std::cout << q;
+}
