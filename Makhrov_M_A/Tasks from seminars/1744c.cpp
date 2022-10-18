ad5343e6a302dcf65f0ffbb0748255ec06ce6ce0
MatveyMakhrov <113934714+MatveyMakhrov@users.noreply.github.com>
2022-10-18 15:46:00 +0300

    Create 1744c.cpp

---
+++ Makhrov_M_A/Tasks from seminars/1744c.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int t(0), n(0);
+    char c(0);
+    std::cin >> t;
+    std::string s;
+    for (int i(0); i < t; i++) {
+        std::cin >> n >> c >> s;
+        for (int j(0); j < n; j++) {
+
+        }
+
+    }
+}
