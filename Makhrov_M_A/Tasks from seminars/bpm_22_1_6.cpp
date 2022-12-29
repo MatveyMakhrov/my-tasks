34bac6b968f1feb5d5b6c49b7e8429d0bc34baf5
MatveyMakhrov <113934714+MatveyMakhrov@users.noreply.github.com>
2022-12-29 12:51:30 +0300

    Update bpm_22_1_6.cpp

--- Makhrov_M_A/Tasks from seminars/bpm_22_1_6.cpp
+++ Makhrov_M_A/Tasks from seminars/bpm_22_1_6.cpp
@@ -1,62 +1,29 @@
 #include <iostream>
+#include <array>
 
-int main() {
+int main()
+{
     int h(0), w(0), x(0), y(0);
     std::cin >> h >> w >> x >> y;
-    for (int i = 0; i < h; i++) {
-        for (int j = 0; j < w; j++) {
-            if (y == i) {
-                if (x == j) {
-                    std::cout << "0";
-                }
+    for (int i = 1; i <= h; i += 1) {
+        for (int j = 1; j <= w; j += 1) {
+            if (std::abs(x - j) == std::abs(y - i)) {
+                std::cout << ((std::abs(x - j) + std::abs(y - i)) / 2) << ' ';
             }
-            if (i + 1 == y) {
-                if (j - 1 == x) {
-                    std::cout << 1;
-                }
-                if (j == x) {
-                    std::cout << 1;
-                }
-                if (j + 1 == x) {
-                    std::cout << 1 << std::endl;
-                }
+            else if (i == y && j == x) {
+                std::cout << 0 << ' ';
             }
-            if (i == y) {
-                if (j - 1 == x) {
-                    std::cout << 1;
-                }
-                if (j + 1 == x) {
-                    std::cout << 1 << std::endl;
-                }
+            else if (i == y && j != x) {
+                std::cout << std::abs(x - j) << ' ';
             }
-            if (i - 1 == y) {
-                if (j - 1 == x) {
-                    std::cout << 1;
-                }
-                if (j == x) {
-                    std::cout << 1;
-                }
-                if (j + 1 == x) {
-                    std::cout << 1 << std::endl;
-                }
+            else if (j == x && i != y) {
+                std::cout << std::abs(y - i) << ' ';
             }
-            if (i - 2 == y) {
-                if (j - 2 == y) {
-                    std::cout << 2;
-                }
-                if (j - 1 == y) {
-                    std::cout << 2;
-                }
-                if (j == y) {
-                    std::cout << 2;
-                }
-                if (j + 1 == y) {
-                    std::cout << 2;
-                }
-                if (j + 2 == y) {
-                    std::cout << 2 << std::endl;
-                }
+            else {
+                std::cout << std::max(std::abs(j - x), std::abs(i - y)) << ' ';
             }
         }
+        std::cout << std::endl;
     }
+    return 0;
 }
